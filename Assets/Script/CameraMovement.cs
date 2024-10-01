f41c0cc4ef59064bc9719ed353d932449acd4aff
unrik76 <unrik76@gmail.com>
2024-10-01 20:30:29 +0200

    Begining

---
+++ Assets/Script/CameraMovement.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    [Header("References")]
+
+    // Update is called once per frame
+    void Update()
+    {
+        Look();
+    }
+    void Look(){
+        this.transform.rotation;
+        }
+}
