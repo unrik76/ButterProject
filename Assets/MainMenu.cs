e569dc45da3af2cb1b54d12d697d36e4c02a2737
polapika <pola.pituch@gmail.com>
2025-02-10 18:34:53 +0100

    Menu fix
    menu fix

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -10,4 +10,9 @@ public class MainMenu : MonoBehaviour
     {
         SceneManager.LoadSceneAsync(1);
     }
+    public void QuitGame()
+    {
+      //  Debug.Log("dziala");
+        Application.Quit();
+    }
 }
